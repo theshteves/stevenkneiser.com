0fdc38fdc1ce8935be14a730ecdc2323955f9a7d
theshteves <stevenkneiser@gmail.com>
2019-04-24 03:09:53 -0400

    Unnecessary file cleanup

--- server/manage.py
+++ server/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.chdir(os.path.dirname(__file__)) # Run from the project root
+    os.chdir(os.path.dirname(os.path.abspath(__file__))) # Run from the project root
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
