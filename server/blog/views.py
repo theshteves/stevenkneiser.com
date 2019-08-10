b18ca60f8e07d57da7f0cf284c13917f4a4f4198
theshteves <stevenkneiser@gmail.com>
2019-08-09 22:28:36 -0400

    Made space for Jekyll skeleton

--- server/blog/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    return HttpResponse('Under construction')
-
-def react(request):
-    context = {'msg': 'Hello World'}
-    return render(request, 'react.html', context)
