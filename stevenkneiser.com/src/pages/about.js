019c9413e2f6b06b66c89db7bac8c81a0c352cdf
theshteves <stevenkneiser@gmail.com>
2021-08-01 03:35:29 -0400

    Added Gatsby skeleton with TailwindUI navbar

---
+++ stevenkneiser.com/src/pages/about.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import NavBar from '../components/navbar'
+
+export default function AboutPage() {
+  return (
+    <>
+      <NavBar />
+
+      <main>
+        <title>About Me</title>
+        <h1>About Me</h1>
+        <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      </main>
+    </>
+  )
+}
---
+++ stevenkneiser.com/src/pages/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import NavBar from '../components/navbar'
+
+const IndexPage = () => {
+  return (
+    <>
+      <NavBar />
+
+      <main>
+        <title>Home Page</title>
+        <h1>Welcome to my Gatsby site!</h1>
+        <p>I'm making this by following the Gatsby Tutorial.</p>
+      </main>
+    </>
+  )
+}
+
+export default IndexPage
