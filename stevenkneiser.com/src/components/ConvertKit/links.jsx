d00e906502681c68648e520eabbd898724c26c05
theshteves <stevenkneiser@gmail.com>
2021-08-15 06:36:18 -0400

    Architected a better two-options approach for working with ConvertKit: custom vs no-code

---
+++ stevenkneiser.com/src/components/ConvertKit/links.jsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+
+
+export default function Links() {
+	return <script async data-uid="cccbfe8a36" src="https://chipper-builder-645.ck.page/cccbfe8a36/index.js"></script>
+}
