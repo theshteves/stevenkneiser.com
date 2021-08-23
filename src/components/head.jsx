64ed24db7f6bba51a2dbcf1ec9e0f918cb215be5
theshteves <stevenkneiser@gmail.com>
2021-08-23 00:36:22 -0400

    Properly linked all icons & manifest

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -24,79 +24,83 @@ export default function Head() {
       <link
         rel='apple-touch-icon'
         sizes='57x57'
-        href='../assets/ico/apple-icon-57x57.png'
+        href='/ico/apple-icon-57x57.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='60x60'
-        href='../assets/ico/apple-icon-60x60.png'
+        href='/ico/apple-icon-60x60.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='72x72'
-        href='../assets/ico/apple-icon-72x72.png'
+        href='/ico/apple-icon-72x72.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='76x76'
-        href='../assets/ico/apple-icon-76x76.png'
+        href='/ico/apple-icon-76x76.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='114x114'
-        href='../assets/ico/apple-icon-114x114.png'
+        href='/ico/apple-icon-114x114.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='120x120'
-        href='../assets/ico/apple-icon-120x120.png'
+        href='/ico/apple-icon-120x120.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='144x144'
-        href='../assets/ico/apple-icon-144x144.png'
+        href='/ico/apple-icon-144x144.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='152x152'
-        href='../assets/ico/apple-icon-152x152.png'
+        href='/ico/apple-icon-152x152.png'
       />
       <link
         rel='apple-touch-icon'
         sizes='180x180'
-        href='../assets/ico/apple-icon-180x180.png'
+        href='/ico/apple-icon-180x180.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='192x192'
-        href='../assets/ico/android-icon-192x192.png'
+        href='/ico/android-icon-192x192.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='32x32'
-        href='../assets/ico/favicon-32x32.png'
+        href='/ico/favicon-32x32.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='96x96'
-        href='../assets/ico/favicon-96x96.png'
+        href='/ico/favicon-96x96.png'
       />
       <link
         rel='icon'
         type='image/png'
         sizes='16x16'
-        href='../assets/ico/favicon-16x16.png'
+        href='/ico/favicon-16x16.png'
       />
-      <link rel='manifest' href='../assets/ico/manifest.json' />
-      <meta name='msapplication-TileColor' content='#ded5c4' />
+
+      <link rel='manifest' href='/manifest.json' />
+      <meta name='theme-color' content='#fffbeb' />
+      {/*<meta name='theme-color' content='#ded5c4' />*/}
+
+      <meta name='msapplication-TileColor' content='#fffbeb' />
+      {/*<meta name='msapplication-TileColor' content='#ded5c4' />*/}
       <meta
         name='msapplication-TileImage'
-        content='../assets/ico/ms-icon-144x144.png'
+        content='/ico/ms-icon-144x144.png'
       />
-      <meta name='theme-color' content='#ded5c4' />
     </Helmet>
   )
 }
