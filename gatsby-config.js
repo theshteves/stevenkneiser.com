6003d8fd8c3e5871a75130d9d5d8e7c02a2a0055
theshteves <stevenkneiser@gmail.com>
2021-09-04 14:58:40 -0400

    Added RSS feed & updates all dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,59 @@ module.exports = {
       },
       __key: 'blog',
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.frontmatter.permalink,
+                  guid: site.siteMetadata.siteUrl + edge.node.frontmatter.permalink,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+												permalink
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "RSS Feed for stevenkneiser.com",
+          },
+        ],
+      },
+    },
   ],
   flags: {
     DEV_SSR: true,
