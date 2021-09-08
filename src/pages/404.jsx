42c78ab6625c5b1eefd6bf742d3c2b2dcfcf4bd8
theshteves <stevenkneiser@gmail.com>
2021-09-07 23:43:26 -0400

    Finally corrected /404 (broken during migration)

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -4,15 +4,20 @@ import { Link } from 'gatsby'
 import * as notFoundStyles from '../styles/404.module.css'
 
 export default function NotFoundPage() {
+  const goBack = () => {
+    const history = typeof window !== 'undefined' ? window.history : '' // React workaround to access window
+    history.back()
+  }
+
   return (
     <div className={notFoundStyles.notFound}>
-      <div className='noise fullscreen' />
-      <div className='overlay fullscreen' />
-      <div className='terminal'>
+      <div className={notFoundStyles.noise} />
+      <div className={notFoundStyles.overlay} />
+      <div className={notFoundStyles.terminal}>
         <h1>
-          Error <span className='errorcode'>404</span>
+          Error <span className={notFoundStyles.errorcode}>404</span>
         </h1>
-        <p className='output'>
+        <p className={notFoundStyles.output}>
           The page you&apos;re looking for
           <br />
           might have been removed,
@@ -21,12 +26,15 @@ export default function NotFoundPage() {
           <br />
           or is temporarily unavailable
         </p>
-        <p className='output'>
-          Please try to <a href='javascript:history.back()'>go back</a>
+        <p className={notFoundStyles.output}>
+          Please try to{' '}
+          <button type='button' onClick={goBack} onKeyDown={goBack}>
+            go back
+          </button>
           <br />
           or <Link to='/'>return to the homepage</Link>
         </p>
-        <p className='output'>Good luck</p>
+        <p className={notFoundStyles.output}>Good luck</p>
       </div>
     </div>
   )
