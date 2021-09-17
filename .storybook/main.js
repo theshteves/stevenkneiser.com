9d2acca9eb065171a75dc25bed6fd6767a01be9c
theshteves <stevenkneiser@gmail.com>
2021-09-17 04:30:56 -0400

    Integrated Gatsby-flavored Storybooks

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,8 +8,22 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-docs",
     "storybook-addon-gatsby",
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        cssLoaderOptions: {
+          // When you have splitted your css over multiple files
+          // and use @import('./other-styles.css')
+          importLoaders: 1,
+        },
+        postcssLoaderOptions: {
+          // When using postCSS 8
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
   "core": {
     "builder": "webpack5",
-  }
+  },
 }
