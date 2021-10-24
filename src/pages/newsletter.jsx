2ef7d0b644847eeae89b83a7cae7dc3c4cf6da43
theshteves <stevenkneiser@gmail.com>
2021-10-24 15:34:38 -0400

    Quietly publishing /newsletter: the eventual long-copy for joining my newsletter

---
+++ src/pages/newsletter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Embed from '../components/ConvertKit/embed'
+
+export default function NewsletterPage() {
+  // const url = typeof window !== 'undefined' ? window.location.href : ''
+  const meta = {
+    title: 'Newsletter - Steven Kneiser',
+    description: `Join us. Every Sunday, I send a brief summary of the most fascinating stuff I find around the web (e.g. the future of work, software, business, etc).`,
+  }
+
+  return (
+    <Layout meta={meta}>
+      <h1>Hear "what&apos;s new"<br />every week</h1>
+
+      <p>
+        Every Sunday, I send a brief summary
+        <span className='md:hidden'>
+          <br />
+        </span>{' '}
+        of the most fascinating stuff
+        <span className='md:hidden'>
+          <br />
+        </span>{' '}
+        I find around the web
+      </p>
+      <br />
+
+      <Embed className='mx-0' />
+    </Layout>
+  )
+}
